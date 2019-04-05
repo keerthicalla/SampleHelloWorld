55bf1e8f5e1cf7e13976ea9dd85065f882325d42
kalla <kalla@KALLA>
2019-04-05 08:54:33 -0500

    Added new class file

---
+++ HelloWorldExample/HelloWorldExample/Sample2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldExample
+{
+    class Sample2
+    {
+        static void main()
+        {
+            int num = 0;
+            Console.WriteLine("Enter integer Value");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("num = " + num);
+            Console.ReadKey();
+        }
+    }
+}
