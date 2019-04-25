95cb3be78cf3ca33eebafc5bc2538e433decf6d3
kalla <kalla@KALLA>
2019-04-25 13:18:50 -0500

    Added sample file

---
+++ HelloWorldExample/HelloWorldExample/Samplefileforsourcetree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldExample
+{
+    class Samplefileforsourcetree
+    {
+        static void main()
+        {
+            Console.WriteLine("Hello World Sample Example");
+            Console.ReadKey();
+        }
+    }
+}
