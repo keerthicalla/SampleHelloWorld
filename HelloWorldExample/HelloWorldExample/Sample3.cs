1daae8fba2b333fb6efb84e1bcab2db9b63da125
kalla <kalla@KALLA>
2019-05-03 14:34:02 -0500

    Added a line of code

--- HelloWorldExample/HelloWorldExample/Sample3.cs
+++ HelloWorldExample/HelloWorldExample/Sample3.cs
@@ -16,6 +16,7 @@ namespace HelloWorldExample
             num2 = Convert.ToInt32(Console.ReadLine());
             num3 = num1 + num2;
             Console.WriteLine("Total is " + num3);
+            Console.ReadKey();
             
         }
     }
