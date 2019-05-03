b761528bc780b995f8209a2bf41a09b1f78289ca
kalla <kalla@KALLA>
2019-05-03 14:28:24 -0500

    New class file added

---
+++ HelloWorldExample/HelloWorldExample/Sample3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldExample
+{
+    class Sample3
+    {
+        static void main()
+        {
+            int num1, num2,num3;
+            Console.WriteLine("Enter integer Value");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = num1 + num2;
+            Console.WriteLine("Total is " + num3);
+            
+        }
+    }
+}
