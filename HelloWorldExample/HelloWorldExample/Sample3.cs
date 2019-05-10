b888b10b77f7bd5e828648e553fd237db976082a
kalla <kalla@KALLA>
2019-05-10 12:19:28 -0500

    SAM-2 Deleted lines

--- HelloWorldExample/HelloWorldExample/Sample3.cs
+++ HelloWorldExample/HelloWorldExample/Sample3.cs
@@ -10,12 +10,7 @@ namespace HelloWorldExample
     {
         static void main()
         {
-            int num1, num2,num3;
-            Console.WriteLine("Enter integer Value");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            num2 = Convert.ToInt32(Console.ReadLine());
-            num3 = num1 + num2;
-            Console.WriteLine("Total is " + num3);
+            
             Console.WriteLine("This is the sample file for Testing JIRA workflow triggers");
             Console.ReadKey();
             
