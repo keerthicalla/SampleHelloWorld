e48eb9022ef5f56a17e8164dc49b443c169adc73
kalla <kalla@KALLA>
2019-05-13 12:37:36 -0500

    SAM-3 Removed space

--- HelloWorldExample/HelloWorldExample/Sample3.cs
+++ HelloWorldExample/HelloWorldExample/Sample3.cs
@@ -9,8 +9,7 @@ namespace HelloWorldExample
     class Sample3
     {
         static void main()
-        {
-            
+        {            
             Console.WriteLine("This is the sample file for Testing JIRA workflow triggers");
             Console.WriteLine("This commit is to check how the workflow trigger is working when we create pull request");
             Console.WriteLine("This commit is to check workflow with Approvals");
