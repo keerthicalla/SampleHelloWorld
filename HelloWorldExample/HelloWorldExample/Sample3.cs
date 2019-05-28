35a465f9a1b97caf8be89895dfea9365ad526686
kalla <kalla@KALLA>
2019-05-28 09:30:47 -0500

    KSH-4 Removed line

--- HelloWorldExample/HelloWorldExample/Sample3.cs
+++ HelloWorldExample/HelloWorldExample/Sample3.cs
@@ -12,7 +12,6 @@ namespace HelloWorldExample
         {
             int a, b, c;
             Console.WriteLine("This is to test workflow with Triggers and conditions(Tester approval)");
-            Console.WriteLine("Sample line of code:");
             Console.WriteLine("enter any number");
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
